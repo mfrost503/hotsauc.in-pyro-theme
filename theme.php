ae5fd7c986f12e5b3befe38498d1c9fa3a13f732
Matt Frost <mfrost.design@gmail.com>
2013-11-21 12:46:49 -0500

    Initial theme commit

---
+++ theme.php
@@ -0,0 +1,14 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Class_Bootstrap extends Theme {
+
+    public $name = 'bootstrap';
+    public $author = 'Matt Frost';
+    public $author_website = 'http://shortwhitebaldguy.com';
+    public $website = '';
+    public $description = 'Twitter Bootstrap';
+    public $version = '1.0.0';
+
+}
+
+/* End of file theme.php */
